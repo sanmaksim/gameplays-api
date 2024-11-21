20dff844a22591388f97e8c0bf043a78871627c6
Sandra Maksimovic <sanmaksimovic@gmail.com>
2024-11-21 16:15:06 +1100

    simplified vite config

--- GameplaysFrontend/vite.config.ts
+++ GameplaysFrontend/vite.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-let serverTarget = "";
-
-if (process.env.NODE_ENV === 'development') {
-  serverTarget = "https://localhost:5001";
-} else {
-  serverTarget = "https://localhost:8001";
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: serverTarget,
-        changeOrigin: true
-      }
-    }
+    port: 3000
   }
 });
