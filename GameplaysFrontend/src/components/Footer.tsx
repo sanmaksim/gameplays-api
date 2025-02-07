68254637194d4aa26d9b2a1820c2fc1b760675a7
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-02-07 16:35:45 +1100

    updated footer to inlcude Giant Bomb API credit

--- GameplaysFrontend/src/components/Footer.tsx
+++ GameplaysFrontend/src/components/Footer.tsx
@@ -6,7 +6,10 @@ function Footer() {
     return (
         <div className="container mt-auto">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-body-secondary">&copy; {currentYear} Gameplays. All rights reserved.</p>
+                <p className="col-md-4 mb-0 text-body-secondary">
+                    <div>&copy; {currentYear} Gameplays. All rights reserved.</div>
+                    <div>Game data provided by <Link target="_blank" to="http://www.giantbomb.com">GiantBomb's</Link> <Link target="_blank" to="http://www.giantbomb.com/api/">API</Link></div>
+                </p>
                 
                 <ul className="nav col-md-4 justify-content-end">
                     <li className="nav-item">
