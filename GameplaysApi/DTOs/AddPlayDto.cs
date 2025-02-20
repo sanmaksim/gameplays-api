2978143c94c5975f0091fa8ce72ddaad70b69f02
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-02-20 21:09:07 +1100

    adding new Play DTO

---
+++ GameplaysApi/DTOs/AddPlayDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GameplaysApi.DTOs
+{
+    public class AddPlayDto
+    {
+        public required int UserId { get; set; }
+
+        public required int GameId { get; set; }
+
+        public required int Status { get; set; }
+    }
+}
