144038f1d0eb462072b7f0e0f98593825de09751
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-03-16 17:46:27 +1100

    Update application db model context
    Added fixes related to updated models and additional tables; updated
    User and Game table configs; added Game junction table configs

--- GameplaysApi/Data/ApplicationDbContext.cs
+++ GameplaysApi/Data/ApplicationDbContext.cs
@@ -6,23 +6,72 @@ namespace GameplaysApi.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-
-        }
-
-        public DbSet<User> Users { get; set; }
-
+        { }
+        
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Franchise> Franchises { get; set; }
         public DbSet<Game> Games { get; set; }
-
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
         public DbSet<Play> Plays { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             
             modelBuilder.Entity<User>()
-                .HasIndex(u => u.Email)
-                .IsUnique();
+                .HasAlternateKey(u => u.Email);
+
+            modelBuilder.Entity<Game>(entity =>
+            {
+                entity.HasAlternateKey(g => g.GameId);
+
+                entity.Property(g => g.Image)
+                    .HasColumnType("json");
+
+                // junction table customizations
+                entity.HasMany(g => g.Developers)
+                    .WithMany(f => f.Games)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "GamesDevelopers",
+                        j => j.HasOne<Developer>().WithMany().HasForeignKey("DeveloperId"),
+                        j => j.HasOne<Game>().WithMany().HasForeignKey("GameId")
+                    );
+
+                entity.HasMany(g => g.Franchises)
+                    .WithMany(f => f.Games)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "GamesFranchises",
+                        j => j.HasOne<Franchise>().WithMany().HasForeignKey("FranchiseId"),
+                        j => j.HasOne<Game>().WithMany().HasForeignKey("GameId")
+                    );
+
+                entity.HasMany(g => g.Genres)
+                    .WithMany(f => f.Games)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "GamesGenres",
+                        j => j.HasOne<Genre>().WithMany().HasForeignKey("GenreId"),
+                        j => j.HasOne<Game>().WithMany().HasForeignKey("GameId")
+                    );
+
+                entity.HasMany(g => g.Platforms)
+                    .WithMany(f => f.Games)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "GamesPlatforms",
+                        j => j.HasOne<Platform>().WithMany().HasForeignKey("PlatformId"),
+                        j => j.HasOne<Game>().WithMany().HasForeignKey("GameId")
+                    );
+
+                entity.HasMany(g => g.Publishers)
+                    .WithMany(f => f.Games)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "GamesPublishers",
+                        j => j.HasOne<Publisher>().WithMany().HasForeignKey("PublisherId"),
+                        j => j.HasOne<Game>().WithMany().HasForeignKey("GameId")
+                    );
+            });
         }
     }
 }
