99bee301d455bf4597eb13b8dbe7e6b7e1263d20
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-05-12 14:37:03 +1000

    Add migrations and snapshot for ApiGameId Plays column update

--- GameplaysApi/Migrations/20250512033334_AddApiGameIdColumnToPlays.Designer.cs
+++ GameplaysApi/Migrations/20250512033334_AddApiGameIdColumnToPlays.Designer.cs
@@ -4,6 +4,7 @@ using GameplaysApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameplaysApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250512033334_AddApiGameIdColumnToPlays")]
+    partial class AddApiGameIdColumnToPlays
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GameplaysApi/Migrations/20250512033334_AddApiGameIdColumnToPlays.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameplaysApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApiGameIdColumnToPlays : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApiGameId",
+                table: "Plays",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiGameId",
+                table: "Plays");
+        }
+    }
+}
