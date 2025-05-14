a87bd783234ef5e34059b3dbc4009e5877be4d55
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-05-14 17:55:54 +1000

    Add DTO for returning the playId and status of a game

---
+++ GameplaysApi/DTOs/PlayStatusDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GameplaysApi.DTOs
+{
+    public class PlayStatusDto
+    {
+        [JsonPropertyName("playId")]
+        public int PlayId { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+    }
+}
