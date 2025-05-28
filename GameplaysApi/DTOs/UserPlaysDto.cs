f0b041c2cf517aacfa3a6e33bf7b0f6f5129c848
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-05-28 13:13:22 +1000

    Add new DTO to output data from plays returned by user ID

---
+++ GameplaysApi/DTOs/UserPlaysDto.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GameplaysApi.DTOs
+{
+    public class UserPlaysDto
+    {
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("developers")]
+        public List<DevDto>? Developers { get; set; }
+
+        [JsonPropertyName("original_release_date")]
+        public DateOnly? OriginalReleaseDate { get; set; }
+        
+        [JsonPropertyName("created_at")]
+        public DateOnly CreatedAt { get; set; }
+
+        [JsonPropertyName("hours_played")]
+        public decimal? HoursPlayed { get; set; }
+
+        [JsonPropertyName("percentage_completed")]
+        public decimal? PercentageCompleted { get; set; }
+
+        [JsonPropertyName("last_played_at")]
+        public DateOnly? LastPlayedAt { get; set; }
+    }
+
+    public class DevDto
+    {
+        [JsonPropertyName("id")]
+        public int? DevId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+    }
+}
