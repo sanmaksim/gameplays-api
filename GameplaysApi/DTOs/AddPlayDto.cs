dde26fb4d6de0832c2c9c65949ffda715f3e6584
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-06-18 19:54:32 +1000

    Update variable type to conform to controller action update

--- GameplaysApi/DTOs/AddPlayDto.cs
+++ GameplaysApi/DTOs/AddPlayDto.cs
@@ -2,7 +2,7 @@
 {
     public class AddPlayDto
     {
-        public required int UserId { get; set; }
+        public required string UserId { get; set; }
 
         public required string GameId { get; set; }
 
