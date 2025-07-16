16e8d62deac7a276e16ed34798dbc38aaafafb57
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-07-16 15:08:26 +1000

    Add refresh token model

---
+++ GameplaysApi/Models/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameplaysApi.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [MaxLength(255)]
+        public required string Token { get; set; }
+        public int UserId { get; set; }
+        
+        [ForeignKey(nameof(UserId))]
+        public User? User { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
