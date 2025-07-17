42f5ac2894ff394d91d7aeabac723adff7bcad7c
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-07-17 15:43:57 +1000

    Add create refresh token cookie method
    This method creates a refresh token cookie based on a randomly generated
    token which is hashed and stored in the db per user; Cookie config is
    read from the env; Injected the relevant services via DI; Updated
    existing error msgs for clarity; Re-arranged class fields for
    readability; Added extra null checks for additional env configs such as
    cookie names and expirie times; Simplified the get current user by ID
    return for readability

--- GameplaysApi/Interfaces/IAuthService.cs
+++ GameplaysApi/Interfaces/IAuthService.cs
@@ -5,6 +5,7 @@ namespace GameplaysApi.Interfaces
     public interface IAuthService
     {
         void CreateAuthCookie(User user, HttpResponse response);
+        Task CreateRefreshTokenCookie(User user, HttpRequest request, HttpResponse response);
         void DeleteAuthCookie(HttpResponse response);
         string GetCurrentUserId();
     }
--- GameplaysApi/Services/AuthService.cs
+++ GameplaysApi/Services/AuthService.cs
@@ -10,65 +10,96 @@ namespace GameplaysApi.Services
         private readonly IHttpContextAccessor _contextAccessor;
         private readonly ICookieService _cookieService;
         private readonly IJwtTokenService _jwtTokenService;
-        private readonly string _cookieName = "jwt";
-        
+        private readonly IRefreshTokenService _refreshTokenService;
+
+        private readonly string? _jwtCookieName = Environment.GetEnvironmentVariable("JWT_COOKIE_NAME");
+        private readonly string? _jwtExpirationMinutes = Environment.GetEnvironmentVariable("JWT_EXPIRATION_MINUTES");
+
+        private readonly string? _refreshTokenCookieName = Environment.GetEnvironmentVariable("REFRESH_TOKEN_COOKIE_NAME");
+        private readonly string? _refreshTokenExpirationDays = Environment.GetEnvironmentVariable("REFRESH_TOKEN_EXPIRATION_DAYS");
+
+        private readonly string? _validIssuer = Environment.GetEnvironmentVariable("GAMEPLAYS_VALIDISSUERS");
+        private readonly string? _validAudience = Environment.GetEnvironmentVariable("GAMEPLAYS_VALIDAUDIENCES");
+
         public AuthService(
             IHttpContextAccessor contextAccessor,
             ICookieService cookieService,
-            IJwtTokenService jwtTokenService)
+            IJwtTokenService jwtTokenService,
+            IRefreshTokenService refreshTokenService)
         {
             _contextAccessor = contextAccessor;
             _cookieService = cookieService;
             _jwtTokenService = jwtTokenService;
-
+            _refreshTokenService = refreshTokenService;
         }
 
         public void CreateAuthCookie(User user, HttpResponse response)
         {
-            var expirationMinutes = Environment.GetEnvironmentVariable("EXPIRATION_MINUTES");
-            var validIssuer = Environment.GetEnvironmentVariable("GAMEPLAYS_VALIDISSUERS");
-            var validAudience = Environment.GetEnvironmentVariable("GAMEPLAYS_VALIDAUDIENCES");
-            
-            if (validIssuer != null && validAudience != null)
+            if (_validIssuer != null && _validAudience != null && _jwtCookieName != null)
             {
                 // create JWT
                 var payload = new List<Claim>
                 {
                     new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                    new Claim(JwtRegisteredClaimNames.Iss, validIssuer),
-                    new Claim(JwtRegisteredClaimNames.Aud, validAudience)
+                    new Claim(JwtRegisteredClaimNames.Iss, _validIssuer),
+                    new Claim(JwtRegisteredClaimNames.Aud, _validAudience)
                 };
 
-                if (int.TryParse(expirationMinutes, out int expMins))
+                if (int.TryParse(_jwtExpirationMinutes, out int expirationMinutes))
                 {
-                    var token = _jwtTokenService.CreateToken(payload, expMins);
+                    var expirationMins = TimeSpan.FromMinutes(expirationMinutes);
+                    var jwt = _jwtTokenService.CreateToken(payload, expirationMins);
 
                     // create cookie
-                    var cookieValue = token;
-                    _cookieService.CreateCookie(response, _cookieName, cookieValue, expMins);
+                    var cookieValue = jwt;
+                    _cookieService.CreateCookie(response, _jwtCookieName, cookieValue, expirationMins);
                 }
             }
             else
             {
-                throw new Exception("Audience value must not be null.");
+                throw new Exception("Error reading configuration.");
+            }
+        }
+
+        public async Task CreateRefreshTokenCookie(User user, HttpRequest request, HttpResponse response)
+        {
+            if (_validIssuer != null && _validAudience != null && _refreshTokenCookieName != null
+                && int.TryParse(_refreshTokenExpirationDays, out int expirationDays))
+            {
+                var expDays = TimeSpan.FromDays(expirationDays);
+
+                // create refresh token
+                var refreshToken = await _refreshTokenService.CreateRefreshToken(user, request, expDays);
+
+                // create cookie
+                var cookieValue = refreshToken;
+                _cookieService.CreateCookie(response, _refreshTokenCookieName, cookieValue, expDays);
+                
+            }
+            else
+            {
+                throw new Exception("Error reading configuration.");
             }
         }
 
         public void DeleteAuthCookie(HttpResponse response)
         {
-            _cookieService.DeleteCookie(response, _cookieName);
+            if (_jwtCookieName != null)
+            {
+                _cookieService.DeleteCookie(response, _jwtCookieName);
+            }
+            else
+            {
+                throw new Exception("Error reading configuration.");
+            }
         }
 
         public string GetCurrentUserId()
         {
             // Retrieve the user ID string from the JWT 'sub' claim
             var userId = _contextAccessor.HttpContext?.User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
-            if (userId == null)
-            {
-                throw new InvalidOperationException("Invalid user.");
-            }
-            return userId;
+            return userId == null ? throw new InvalidOperationException("Invalid user.") : userId;
         }
     }
 }
