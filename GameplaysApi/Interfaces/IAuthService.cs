ca1d6b25e86c88f9c14e5c4892cceafcb0b42ecd
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-07-26 12:17:03 +1000

    Revert delete refresh token method signature back to previous

--- GameplaysApi/Interfaces/IAuthService.cs
+++ GameplaysApi/Interfaces/IAuthService.cs
@@ -9,6 +9,6 @@ namespace GameplaysApi.Interfaces
         void DeleteAuthCookie(HttpResponse response);
         Task CreateRefreshTokenCookie(User user, HttpRequest request, HttpResponse response);
         Task UpdateRefreshTokenCookie(User user, HttpRequest request, HttpResponse response, RefreshToken refreshToken);
-        void DeleteRefreshTokenCookie(HttpResponse response, RefreshToken refreshToken);
+        void DeleteRefreshTokenCookie(HttpResponse response);
     }
 }
