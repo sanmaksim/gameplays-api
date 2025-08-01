b5983e8f73f94e4073d275cb428f9342f589da1a
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-01 11:47:48 +1000

    Remove extraneous using line

--- GameplaysApi/Program.cs
+++ GameplaysApi/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Annotations;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.RateLimiting;
