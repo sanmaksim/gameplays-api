d687ea2c12c642bdbd0ee2e73443597b5ae7b1ff
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-02 20:12:20 +1000

    Add new user service and interface
    This is required to move password hashing and verification functionality
    out of the user model (which is not best practice) and into a service of
    its own for better serviceablity

---
+++ GameplaysApi/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace GameplaysApi.Interfaces
+{
+    public interface IUserService
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string enteredPassword, string storedPassword);
+    }
+}
---
+++ GameplaysApi/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using GameplaysApi.Interfaces;
+
+namespace GameplaysApi.Services
+{
+    public class UserService : IUserService
+    {
+        public string HashPassword(string password)
+        {
+            var hashedPassword = "";
+
+            if (!string.IsNullOrWhiteSpace(password))
+            {
+                hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            }
+            else
+            {
+                throw new ArgumentException("Password cannot be null or whitespace.");
+            }
+
+            return hashedPassword;
+        }
+
+        public bool VerifyPassword(string enteredPassword, string storedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, storedPassword);
+        }
+    }
+}
