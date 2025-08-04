7831872a0e6ed581bd143a74e9359820895bcf72
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-04 19:07:36 +1000

    Create class to hold game config properties from environment

---
+++ GameplaysApi/Config/GameConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameplaysApi.Config
+{
+    public class GameConfig
+    {
+        [Required]
+        public string AppName { get; set; } = default!;
+
+        [Required]
+        public string AppVersion { get; set; } = default!;
+
+        [Required]
+        public string AuthorEmail { get; set; } = default!;
+
+        [Required]
+        public string GiantBombApiKey { get; set; } = default!;
+
+        [Required]
+        public string GiantBombApiUrl { get; set; } = default!;
+
+        [Required]
+        public string GiantBombResourcePrefix { get; set; } = default!;
+    }
+}
