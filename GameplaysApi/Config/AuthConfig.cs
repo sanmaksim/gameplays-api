c9cce8ce90a9bc45be4b0caeb08cd398421235d3
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-05 16:55:55 +1000

    Add auth config class to hold auth environment variables

---
+++ GameplaysApi/Config/AuthConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameplaysApi.Config
+{
+    public class AuthConfig
+    {
+        [Required]
+        public string HmacSecretKey { get; set; } = default!;
+
+        [Required]
+        public string JwtCookieName { get; set; } = default!;
+
+        [Required]
+        public string JwtExpMins { get; set; } = default!;
+
+        [Required]
+        public string RefreshCookieName { get; set; } = default!;
+
+        [Required]
+        public string RefreshExpDays { get; set; } = default!;
+
+        [Required]
+        public string ValidIssuers { get; set; } = default!;
+
+        [Required]
+        public string ValidAudiences { get; set; } = default!;
+    }
+}
