324b670df95aaf4049f144c406db848a2ded950c
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-05 16:56:16 +1000

    Add connection config class to hold connection environment variables

---
+++ GameplaysApi/Config/ConnectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameplaysApi.Config
+{
+    public class ConnectionConfig
+    {
+        [Required]
+        public string ConnectionString { get; set; } = default!;
+
+        [Required]
+        public string DevCertPath { get; set; } = default!;
+
+        [Required]
+        public string HttpsPort { get; set; } = default!;
+
+        [Required]
+        public string ProdCertPath { get; set; } = default!;
+
+        [Required]
+        public string ProdKeyPath { get; set; } = default!;
+    }
+}
