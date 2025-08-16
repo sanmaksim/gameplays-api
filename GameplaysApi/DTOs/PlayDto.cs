aa671cb935c80e43ec0d4c6941885bbf888f258a
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-16 15:15:28 +1000

    Add new Play DTO and associated validator

---
+++ GameplaysApi/DTOs/PlayDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameplaysApi.DTOs
+{
+    public class PlayDto
+    {
+        public int? UserId { get; set; }
+        public int? ApiGameId { get; set; }
+        public int? StatusId { get; set; }
+    }
+}
---
+++ GameplaysApi/Validators/PlayDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using GameplaysApi.DTOs;
+
+namespace GameplaysApi.Validators
+{
+    public class PlayDtoValidator : AbstractValidator<PlayDto>
+    {
+        public PlayDtoValidator()
+        {
+            RuleFor(playDto => playDto)
+                .Must(
+                    playDto => playDto.UserId != null 
+                    || playDto.ApiGameId != null
+                    || playDto.StatusId != null)
+                .WithMessage($"At least one property of {nameof(PlayDto)} must not be null.");
+        }
+    }
+}
