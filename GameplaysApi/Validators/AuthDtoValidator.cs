083c1646c524e31acea1c868b8f5d53155b1586a
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-21 10:54:19 +1000

    Update validation rules for auth input

--- GameplaysApi/Validators/AuthDtoValidator.cs
+++ GameplaysApi/Validators/AuthDtoValidator.cs
@@ -11,11 +11,19 @@ namespace GameplaysApi.Validators
                 .Must(authDto => string.IsNullOrEmpty(authDto.Username) ^ string.IsNullOrEmpty(authDto.Email))
                 .WithMessage("Either Username or Email is required, but not both.");
 
+            RuleFor(x => x.Username)
+                .Matches(@"^[a-zA-Z0-9_\-]+$")
+                .WithMessage("Username must be alphanumeric.")
+                .When(authDto => !string.IsNullOrEmpty(authDto.Username));
+
             RuleFor(authDto => authDto.Email)
                 .EmailAddress()
+                .WithMessage("Must be a valid email address.")
                 .When(authDto => !string.IsNullOrEmpty(authDto.Email));
 
-            RuleFor(authDto => authDto.Password).NotEmpty();
+            RuleFor(authDto => authDto.Password)
+                .NotEmpty()
+                .Matches(@"^[a-zA-Z0-9!@#$%^&*_\-+=.,:? ]+$");
         }
     }
 }
