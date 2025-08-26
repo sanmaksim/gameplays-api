a40551ca129e14306d3a4779c5d18b67e2c4d195
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-26 13:38:00 +1000

    Update db migrations to include added description property text field

---
+++ GameplaysApi/Migrations/20250825094413_AddDescriptionColumnToGames.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameplaysApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionColumnToGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Games",
+                type: "text",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Games");
+        }
+    }
+}
