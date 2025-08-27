3dd0339791be014e3c49bc27037a167edd7e5540
Sandra Maksimovic <sanmaksimovic@gmail.com>
2025-08-27 23:07:31 +1000

    Update app db context snapshot & add ef core scaffolding for updating games deck field to text type

---
+++ GameplaysApi/Migrations/20250827125010_UpdateGamesDeckColumnToTextType.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameplaysApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGamesDeckColumnToTextType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Deck",
+                table: "Games",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Deck",
+                table: "Games",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
